2f6bba4f1156b319b7502febe181a6919587c7f5
takker99 <37929109+takker99@users.noreply.github.com>
2022-11-12 12:18:43 +0900

    :recycle:

--- storage.ts
+++ storage.ts
@@ -157,52 +157,6 @@ export const checkUpdate = async (
   }
 };
 
-async function* fetchProjectStatus(
-  projects: ProjectStatus[],
-): AsyncGenerator<
-  Result<
-    Project & { checked: number },
-    (NotLoggedInError | NotFoundError | NotMemberError) & { project: string }
-  >,
-  void,
-  unknown
-> {
-  // idがあるものとないものとに分ける
-  const projectIds: string[] = [];
-  let newProjects: string[] = [];
-  const checkedMap = new Map<string, number>();
-  for (const project of projects) {
-    if (project.id) {
-      projectIds.push(project.id);
-    } else {
-      newProjects.push(project.project);
-    }
-    checkedMap.set(project.project, project.checked);
-  }
-  const result = await listProjects(projectIds);
-  if (!result.ok) {
-    // log inしていないときは、getProject()で全てのprojectのデータを取得する
-    newProjects = projects.map((project) => project.project);
-  } else {
-    for (const project of result.value.projects) {
-      if (!checkedMap.has(project.name)) continue;
-      yield {
-        ok: true,
-        value: { ...project, checked: checkedMap.get(project.name) ?? 0 },
-      };
-    }
-  }
-  for (const name of newProjects) {
-    const res = await getProject(name);
-    yield res.ok
-      ? {
-        ok: true,
-        value: { ...res.value, checked: checkedMap.get(name) ?? 0 },
-      }
-      : { ok: false, value: { ...res.value, project: name } };
-  }
-}
-
 /** 補完ソースをDBから取得する
  *
  * @param projects 取得したい補完ソースのproject nameのリスト
@@ -331,6 +285,53 @@ interface InvalidProjectStatus {
    * アクセス権のないprojectと存在しないprojectの場合はfalseになる
    */
   isValid: false;
+}
+
+/** projectの情報を一括取得する */
+async function* fetchProjectStatus(
+  projects: ProjectStatus[],
+): AsyncGenerator<
+  Result<
+    Project & { checked: number },
+    (NotLoggedInError | NotFoundError | NotMemberError) & { project: string }
+  >,
+  void,
+  unknown
+> {
+  // idがあるものとないものとに分ける
+  const projectIds: string[] = [];
+  let newProjects: string[] = [];
+  const checkedMap = new Map<string, number>();
+  for (const project of projects) {
+    if (project.id) {
+      projectIds.push(project.id);
+    } else {
+      newProjects.push(project.project);
+    }
+    checkedMap.set(project.project, project.checked);
+  }
+  const result = await listProjects(projectIds);
+  if (!result.ok) {
+    // log inしていないときは、getProject()で全てのprojectのデータを取得する
+    newProjects = projects.map((project) => project.project);
+  } else {
+    for (const project of result.value.projects) {
+      if (!checkedMap.has(project.name)) continue;
+      yield {
+        ok: true,
+        value: { ...project, checked: checkedMap.get(project.name) ?? 0 },
+      };
+    }
+  }
+  for (const name of newProjects) {
+    const res = await getProject(name);
+    yield res.ok
+      ? {
+        ok: true,
+        value: { ...res.value, checked: checkedMap.get(name) ?? 0 },
+      }
+      : { ok: false, value: { ...res.value, project: name } };
+  }
 }
 
 /** DBの補完ソースを更新する */
