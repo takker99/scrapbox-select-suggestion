c3aa8111e57d990111a6bfc8708158d298d78f20
takker99 <37929109+takker99@users.noreply.github.com>
2022-12-08 10:21:59 +0900

    :recycle: fix indent on CSS

--- CSS.tsx
+++ CSS.tsx
@@ -8,26 +8,26 @@ import { h } from "./deps/preact.tsx";
 export const CSS = () => (
   <style>
     {`.container {
-position: absolute;
-margin-top: 14px;
-max-height: 80vh;
-z-index: 301;
+  position: absolute;
+  margin-top: 14px;
+  max-height: 80vh;
+  z-index: 301;
 
-background-color: var(--select-suggest-bg, #111);
-font-family: var(--select-suggest-font-family, "Open Sans", Helvetica, Arial, "Hiragino Sans", sans-serif);
-color: var(--select-suggest-text-color, #eee);
-border-radius: 4px;
+  background-color: var(--select-suggest-bg, #111);
+  font-family: var(--select-suggest-font-family, "Open Sans", Helvetica, Arial, "Hiragino Sans", sans-serif);
+  color: var(--select-suggest-text-color, #eee);
+  border-radius: 4px;
 }
 .candidates {
-max-width: 80vw;
+  max-width: 80vw;
 }
 .projects {
-max-width: 10vw;
-margin-right: 4px;
-display: grid;
-grid-template-rows: repeat(4, 1fr);
-grid-auto-flow: column;
-     direction: rtl;
+  max-width: 10vw;
+  margin-right: 4px;
+  display: grid;
+  grid-template-rows: repeat(4, 1fr);
+  grid-auto-flow: column;
+  direction: rtl;
 }
 .container.candidates > :not(:first-child) {
   border-top: 1px solid var(--select-suggest-border-color, #eee);
@@ -35,37 +35,37 @@ grid-auto-flow: column;
 .container.candidates > *{
   font-size: 11px;
   line-height: 1.2em;
-padding: 0.5em 10px;
+  padding: 0.5em 10px;
 }
 .candidate {
-display: flex;
+  display: flex;
 }
 a {
-display: block;
-         text-decoration: none;
-color: inherit;
+  display: block;
+  text-decoration: none;
+  color: inherit;
 }
 a:not(.mark) {
-width: 100%;
-  }
+  width: 100%;
+}
 .selected a {
   background-color: var(--select-suggest-selected-bg, #222);
   text-decoration: underline
 }
 img {
-height: 1.3em;
-width: 1.3em;
-position: relative;
-          object-fit: cover;
-          object-position: 0% 0%;
+  height: 1.3em;
+  width: 1.3em;
+  position: relative;
+  object-fit: cover;
+  object-position: 0% 0%;
 }
 .disabled {
-filter: grayscale(1.0) opacity(0.5);
+  filter: grayscale(1.0) opacity(0.5);
 }
 .counter {
-color: var(--select-suggest-information-text-color, #aaa);
-       font-size: 80%;
-       font-style: italic;
+  color: var(--select-suggest-information-text-color, #aaa);
+  font-size: 80%;
+  font-style: italic;
 }`}
   </style>
 );
