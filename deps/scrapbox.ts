5ae63c6533fcc61c28eddcb54c9ee73adc0336c0
GitHub <noreply@github.com>
2023-08-14 00:57:31 +0000

    :arrow_up: update deno dependencies

--- deps/scrapbox.ts
+++ deps/scrapbox.ts
@@ -5,10 +5,10 @@ export type {
   Line,
   Node,
   Scrapbox,
-} from "https://raw.githubusercontent.com/scrapbox-jp/types/0.4.2/userscript.ts";
+} from "https://raw.githubusercontent.com/scrapbox-jp/types/0.5.0/userscript.ts";
 export type {
   NotFoundError,
   NotLoggedInError,
   NotMemberError,
   Project,
-} from "https://raw.githubusercontent.com/scrapbox-jp/types/0.4.2/rest.ts";
+} from "https://raw.githubusercontent.com/scrapbox-jp/types/0.5.0/rest.ts";
