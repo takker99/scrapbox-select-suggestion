39ed34eb2dac9cf81440438beb9650763493bedd
GitHub <noreply@github.com>
2023-11-25 00:59:48 +0000

    :arrow_up: update deno dependencies

--- deps/testing.ts
+++ deps/testing.ts
@@ -1,2 +1,2 @@
-export * from "https://deno.land/std@0.206.0/assert/mod.ts";
-export * from "https://deno.land/std@0.206.0/testing/snapshot.ts";
+export * from "https://deno.land/std@0.208.0/assert/mod.ts";
+export * from "https://deno.land/std@0.208.0/testing/snapshot.ts";
