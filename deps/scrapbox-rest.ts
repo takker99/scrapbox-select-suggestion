76f1ac68507c38986c645ac1abbc4f13002e4288
GitHub <noreply@github.com>
2024-06-15 01:06:10 +0000

    :arrow_up: update deno dependencies

--- deps/scrapbox-rest.ts
+++ deps/scrapbox-rest.ts
@@ -2,8 +2,8 @@ export {
   getProject,
   listProjects,
   readLinksBulk,
-} from "https://raw.githubusercontent.com/takker99/scrapbox-userscript-std/0.26.1/rest/mod.ts";
+} from "https://raw.githubusercontent.com/takker99/scrapbox-userscript-std/0.26.2/rest/mod.ts";
 export type {
   Result,
-} from "https://raw.githubusercontent.com/takker99/scrapbox-userscript-std/0.26.1/rest/mod.ts";
-export { toTitleLc } from "https://raw.githubusercontent.com/takker99/scrapbox-userscript-std/0.26.1/title.ts";
+} from "https://raw.githubusercontent.com/takker99/scrapbox-userscript-std/0.26.2/rest/mod.ts";
+export { toTitleLc } from "https://raw.githubusercontent.com/takker99/scrapbox-userscript-std/0.26.2/title.ts";
