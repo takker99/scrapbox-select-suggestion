3b308aafb94ded1f0bbc1bbccf7fd29458d219d6
GitHub <noreply@github.com>
2024-07-15 01:10:34 +0000

    :arrow_up: update deno dependencies

--- deps/scrapbox.ts
+++ deps/scrapbox.ts
@@ -1,14 +1,14 @@
-export * from "https://raw.githubusercontent.com/takker99/scrapbox-userscript-std/0.26.2/browser/dom/mod.ts";
-export * from "https://raw.githubusercontent.com/takker99/scrapbox-userscript-std/0.26.2/title.ts";
+export * from "https://raw.githubusercontent.com/takker99/scrapbox-userscript-std/0.27.0/browser/dom/mod.ts";
+export * from "https://raw.githubusercontent.com/takker99/scrapbox-userscript-std/0.27.0/title.ts";
 export type {
   Candidate,
   Line,
   Node,
   Scrapbox,
-} from "https://raw.githubusercontent.com/scrapbox-jp/types/0.7.1/userscript.ts";
+} from "https://raw.githubusercontent.com/scrapbox-jp/types/0.9.0/userscript.ts";
 export type {
   NotFoundError,
   NotLoggedInError,
   NotMemberError,
   Project,
-} from "https://raw.githubusercontent.com/scrapbox-jp/types/0.7.1/rest.ts";
+} from "https://raw.githubusercontent.com/scrapbox-jp/types/0.9.0/rest.ts";
