c1d7ecdd809966a588bdee1e3aeff700e9b8371f
GitHub <noreply@github.com>
2024-07-21 01:13:44 +0000

    :arrow_up: update deno dependencies

--- deps/storage.ts
+++ deps/storage.ts
@@ -1,5 +1,5 @@
-export * from "https://raw.githubusercontent.com/takker99/scrapbox-storage/0.1.3/mod.ts";
+export * from "https://raw.githubusercontent.com/takker99/scrapbox-storage/0.1.5/mod.ts";
 
-import { deleteDB } from "https://raw.githubusercontent.com/takker99/scrapbox-storage/0.1.3/deps/idb.ts";
+import { deleteDB } from "https://raw.githubusercontent.com/takker99/scrapbox-storage/0.1.5/deps/idb.ts";
 // 旧ver.で使っていたDBを削除する
 deleteDB("userscript-links").catch((e) => console.error(e));
