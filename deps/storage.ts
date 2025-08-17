ab834665dcc627c9330c74044f9cdf2ddfd1e74e
takker99 <37929109+takker99@users.noreply.github.com>
2025-08-17 10:36:24 +0900

    refactor: Remove `deno.ns`, which is not used in the worker code.

--- deps/storage.ts
+++ deps/storage.ts
@@ -2,7 +2,8 @@ export * from "jsr:@takker/cosense-storage@0.3";
 
 import { deleteDB } from "npm:idb@8";
 
-if (!Deno) {
+// deno-lint-ignore no-explicit-any
+if (!(globalThis as any).Deno) {
   // 旧ver.で使っていたDBを削除する
   deleteDB("userscript-links").catch((e) => console.error(e));
 }
